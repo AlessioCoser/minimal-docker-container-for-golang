047d10772a8de65951bf7b668c5051adae9cd62a
Alessio Coser <alessio.coser@xpeppers.com>
2018-03-05 17:07:17 +0100

    adds simple helloworld router

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello World!")
+	port := 8080
+
+	http.HandleFunc("/", helloWorldHandler)
+
+	log.Printf("Server starting on port %v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World\n")
 }
