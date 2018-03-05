1986d063d27a1933d084b9692b8fd226a4ee7620
Alessio Coser <alessio.coser@xpeppers.com>
2018-03-05 17:55:09 +0100

    makes api response in json format

--- main.go
+++ main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	port := 8080
+	port := getEnv("PORT", "8080")
 
-	http.HandleFunc("/", helloWorldHandler)
+	http.HandleFunc("/helloworld", helloWorldHandler)
 
 	log.Printf("Server starting on port %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
-func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World\n")
+func helloWorldHandler(writer http.ResponseWriter, request *http.Request) {
+	log.Printf("Processing request %+v\n", request)
+	writer.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	fmt.Fprint(writer, "{\"greeting\":\"Hello World\"}")
+}
+
+func getEnv(name string, defaultPort string) string {
+	envPort, exists := os.LookupEnv(name)
+	if (!exists) {
+			envPort = defaultPort
+	}
+
+	return envPort;
 }
